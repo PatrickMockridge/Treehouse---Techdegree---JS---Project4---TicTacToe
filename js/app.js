47a6771a8da423ade78b04fe2c96bf6fefe6916f
Patrick Mockridge <patrick@engzig.com>
2016-05-30 19:30:23 +0100

    Added Player Names
    Player Names now appear as stated in ‘Exceeds Expectations’

--- js/app.js
+++ js/app.js
@@ -4,14 +4,21 @@
 // use strict
 'use strict'
 // append start screen div
-$('body').append("<div class='screen screen-start' id='start'><header><h1>Tic Tac Toe</h1><p class='message'></p><a href='#' class='button'>Start game</a></header></div>");
+$('body').append("<div class='screen screen-start' id='start'><header><h1>Tic Tac Toe</h1><br><p class='message'>Enter Player 1 Name: </p><input type='text' class='player1Name' name='player1Name'><br><p class='message'>Enter Player 2 Name: </p><input type='text' class='player2Name' name='player2Name'><br><br><a href='#' class='button start'>Start game</a></header></div>");
 // append player 1 wins screen div
 $('body').append("<div class='screen screen-win-one' id='finish'><header><h1>Tic Tac Toe</h1><p class='message'> Wins!</p><a href='#' class='button'>New game</a></header></div>");
 // append player 2 wins screen div
 $('body').append("<div class='screen screen-win-two' id='finish'><header><h1>Tic Tac Toe</h1><p class='message'> Wins!</p><a href='#' class='button'>New game</a></header></div>");
 // append it's a tie div
 $('body').append("<div class='screen screen-win-draw' id='finish'><header><h1>Tic Tac Toe</h1><p class='message'>It's a Tie!</p><a href='#' class='button'>New game</a></header></div>");
-
+//make text box look nicey
+$('input').css({
+    "width": "30%",
+    "padding": "12px 20px",
+    "margin": "8px 0",
+    "box-sizing": "border-box",
+    "border-radius": "3px"
+});
 //set count variable to zero modulo of count variables decides whose turn it is
 var count = 0;
 //declare all initial box state variables
@@ -62,6 +69,20 @@ $('.screen-win-one').hide();
 $('.screen-win-two').hide();
 $('.screen-win-draw').hide();
 $('.board').hide();
+// if text has been entered for player names, replaced O and X in the board page header with the names entered and change the winning screens
+$('.button').click(function() {
+  var player1Name = $(".player1Name").val();
+  if (player1Name) {
+  $('#player1').html("<p style='color:black;'>" + player1Name + "</p>");
+  $('.screen-win-one').find('.message').html(" " + player1Name + " Wins!");
+  }
+  var player2Name = $(".player2Name").val();
+  if (player2Name) {
+  $('#player2').html("<p style='color:black;'>" + player2Name + "</p>");
+  $('.screen-win-two').find('.message').html(" " + player2Name + " Wins!");
+  }
+});
+
 // clicking on a button either starts or restarts the game
 $('.button').click(function() {
   count = 0;
@@ -180,7 +201,9 @@ $('.box').click(function() {
     }
   checkWin();
   count++;
+  //if all box full and still no winner
   if (!player1Win && !player2Win && (count == 9)) {
+    //it's a tie!
     itsATie();
   }
   return count;
